a3ee308b5860e235a4d01dc7462c856c384b69ff
ModMonster <modmonster2060@gmail.com>
2026-04-26 23:02:20 -0400

    Don't show the prune button on worlds without sync enabled

--- src/main/java/ca/modmonster/minegit/mixin/EditWorldScreenMixin.java
+++ src/main/java/ca/modmonster/minegit/mixin/EditWorldScreenMixin.java
@@ -1,21 +1,20 @@
 package ca.modmonster.minegit.mixin;
 
+import ca.modmonster.minegit.data.GitManager;
+import ca.modmonster.minegit.gui.PruneWorldScreen;
+import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.layouts.LinearLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.worldselection.EditWorldScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.storage.LevelStorageSource;
-
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import ca.modmonster.minegit.gui.PruneWorldScreen;
-import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
 
 @Mixin(EditWorldScreen.class)
 public class EditWorldScreenMixin extends Screen {
@@ -37,9 +36,10 @@ public class EditWorldScreenMixin extends Screen {
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/layouts/LinearLayout;addChild(Lnet/minecraft/client/gui/layouts/LayoutElement;)Lnet/minecraft/client/gui/layouts/LayoutElement;", ordinal = 10), method = "<init>", remap = false)
     private void init(CallbackInfo info) {
+        if (!GitManager.syncEnabled(minecraft, levelAccess.getLevelId())) return;
+
         // Add prune button
         layout.addChild(Button.builder(Component.translatable("minegit.prune.button"), button -> minecraft.setScreen(new PruneWorldScreen(this, levelAccess, callback))).width(200).build());
-
         layout.visitWidgets(this::addRenderableWidget);
     }
 }
